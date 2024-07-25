2caa6ecc43acb04f0ef84839af00676d368cfb95
Maadeha shaikh <maadeham@gmail.com>
2024-07-26 00:18:47 +0500

    updated

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
-import inquirer from "inquirer";
-let user = await inquirer.prompt({
-    name: "age",
-    type: "number",
-    message: "Enter your age"
-});
-console.log(user);
+// import inquirer from "inquirer";
+// let day1 = await inquirer.prompt({
+//   name: "day",
+//   type: "number",
+//   message: "Enter your day"
+// })
+// console.log(day1);
+// let day = "Sunday"
+// switch(day){
+//   case "Sunday":
+//   console.log("Go Hiking Its Sunday");
+//   break;
+//   case "Friday":
+//   console.log("Do Pray Its Friday");
+//   break;
+//   case "Monday":
+//   console.log("Its Office Day Upps!!");
+//   break;
+//   default:
+//     console.log("Enter valid day");
+// }
+// import inquirer from "inquirer";
+// const input1 = await inquirer.prompt({
+//   name:"number1",
+//   type:"number",
+//   message:"Enter your first number here :"
+// })
+// const input2 = await inquirer.prompt({
+//   name:"number2",
+//   type:"number",
+//   message:"Enter your first number here :"
+// })
+// let add :number = input1.number1 + input2.number2 ; //5
+// let sub :number = input1.number1 - input2.number2 ; //1
+// let mult :number = input1.number1 * input2.number2 ; //6
+// let divide :number = input1.number1 / input2.number2 ; //1.5
+// console.log(`Addition's result is ${add} Subtraction result is ${sub} Multiplication result is :${mult} and 
+//   division result is ${divide}`);
+// console.log(add+sub*mult-divide);    
+function halfFryEgg() {
+    let cooked = 2 * 2;
+    return (cooked);
+}
+console.log(halfFryEgg());
+export {};
